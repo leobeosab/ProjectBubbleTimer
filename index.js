c0c9b273d454730652ea5a88230499067bf3e4b3
Ryan Wise <ryan.wise@wiseweb-design.com>
2017-09-20 00:22:58 -0400

    connects to PB api and lists projects and tasks for selected project

--- index.js
+++ index.js
@@ -15,9 +15,54 @@ function init() {
   taskSelect = document.getElementById("task_select");
   subTaskSelect = document.getElementById("subtask_select");
   
+  //Populate projects
+  requestApi("projects", (response) => {
+    for (project of response.data) {
+      if (project.active == "1") {
+          addToSelect(projectSelect, project.project_name, project.project_id);
+      }
+    }
+  });
   
+  //Populate Tasks
+  projectSelect.onchange = () => {
+    subTaskSelect.innerHTML = "";
+    requestApi("tasks", (response) => {
+      for (task of response.data) {
+        addToSelect(taskSelect, task.task_name, task.task_id);
+      }
+    }, [{ name: "project_id", value: projectSelect.value }]);
+  }
 }
 
-function requestApi(request) {
-  var header
+function requestApi(request, callback, getVars) {
+  var headers = new Headers({
+    'key': apiKey,
+    'domain': domain
+  });
+  
+  let url = apiBase + request;
+  
+  if (getVars && getVars.length > 0) {
+    url += "?";
+    for (item of getVars) {
+      url += item.name;
+      url += "=";
+      url += item.value;
+      url += "&";
+    }
+    url = url.slice(0, -1);
+  }
+  
+  fetch(url, {
+    headers: headers
+  }).then((response) => response.json())
+  .then((response) => callback(response))
+  .catch((err) => console.log(err)); 
 }
+
+function addToSelect(select, text, value) {
+  select.options[select.options.length] = new Option(text, value);
+}
+
+document.addEventListener("DOMContentLoaded", () => init());
